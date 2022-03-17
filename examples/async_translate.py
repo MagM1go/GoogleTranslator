417fc91d79c050951782339b23d01fe04cb9cb3b
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-03-17 18:58:19 +0800

    Add files via upload

---
+++ examples/async_translate.py
@@ -0,0 +1,9 @@
+import asyncio
+import GoogleTranslator
+
+translator = GoogleTranslator()
+
+async def my_translator(text, src_lang: str):
+    await translator.translate_async(text, src_lang)
+
+print(asyncio.run(my_translator("test", "ru")))
---
+++ examples/sync_translate.py
@@ -0,0 +1,9 @@
+import asyncio
+import GoogleTranslator
+
+translator = GoogleTranslator()
+
+def my_translator(text, src_lang: str):
+    translator.translate_sync(text, src_lang)
+
+print(my_translator("test", "ru"))
